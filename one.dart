b222794a00aced813f562bfeec8911704d4471ce
amalmohan123 <amalkarikkad@gmail.com>
2023-09-27 10:23:10 +0530

    second commit

--- one.dart
+++ one.dart
@@ -1,5 +1,13 @@
-void main() {
-  
 
+import 'dart:io';
+
+/* 1. Create a program that asks the user to enter their name and their age. 
+    Print out a message that tells how many years they have to be 100 years old. */
+
+void main() {
+  print('Enter the name');
+  String? name = stdin.readLineSync();
+  print('Enter the age');
+  int age = int.parse(stdin.readLineSync()!);
   
 }
