45b2efd6a5ba577732b79018dd3c2a6ff4f924d3
amalmohan123 <amalkarikkad@gmail.com>
2023-09-30 11:46:31 +0530

    five

--- five.dart
+++ five.dart
@@ -9,21 +9,37 @@ and write a program that returns a list that contains only the elements that are
 between them (without duplicates). Make sure your program works on two lists of different sizes. */
 
 void main() {
-  var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-
-  var b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-  var c = [];
-
-  // print( a);
-  // print(a.length);
+    List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
+  List<int> c = [];
 
   for (int i = 0; i < a.length; i++) {
-    for (int j = 1; j < b.length; j++) {
-      if (a[i] == b[i]) {
-        c.add(i);
+    for (int j = 0; j < b.length; j++) {
+      if (a[i] == b[j]) {
+        c.add(a[i]);
       }
     }
   }
-   print("$c[i]"); 
-}
+
+  for (int i = 0; i < c.length; i++) {
+    for (int j = i + 1; j < c.length; j++) {
+      if (c[i] == c[j]) {
+        c.removeAt(j);
+      } else {
+        j++;        
+      }
+    }
+  }
+  }
+
+
+
+//  for (int i = 0; i < a.length; i++) {
+//     for (int j = i; j < b.length; j++) {
+//       if (a[i] == b[i]) {
+//         c.add(a[i]);
+    
+//       }
+//     }
+   
+//   }print(c.toSet()); 
---
+++ six.dart
@@ -0,0 +1,14 @@
+/* 6. Ask the user for a string and print out whether this string is a palindrome or not.
+
+       A palindrome is a string that reads the same forwards and backwards. */
+
+import 'dart:io';
+
+void main() {
+  
+  print('Enter a name ');
+  String? name = stdin.readLineSync();
+  for (int i = 0; i <; i++) {
+    
+  }
+}
