a09ae8439e05fbfea1948d59383752548faa46bb
amalmohan123 <amalkarikkad@gmail.com>
2023-10-24 21:39:33 +0530

    second

--- 1.dart
+++ 1.dart
@@ -12,11 +12,19 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  for (int i = 1; i <= 5; i++) {
-    for (int j = i; j <= 5; j++) {
-      stdout.write("${String.fromCharCode(64+i)}");
-    }
-    stdout.write("\n");
-    print(i);
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = i; j <= 5; j++) {
+  //     stdout.write("${String.fromCharCode(64+i)}");
+  //   }
+  //   stdout.write("\n");
+  //   print(i);
+  // }
+
+for(int i=1;i<=5;i++){
+  for (int j=1;j<=i;j++){
+    stdout.write('8');
   }
+  print('');
+}
+
 }
